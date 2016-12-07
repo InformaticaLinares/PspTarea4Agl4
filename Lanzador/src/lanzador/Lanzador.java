923b90e714527d74f9698e1931b5f660bf23402d
pedrogeas <pedrogeas@msn.com>
2016-12-07 22:30:29 +0100

    Subo el Lanzador. Creo que no necesita nada más que los archivos jar

--- Lanzador/src/lanzador/Lanzador.java
+++ Lanzador/src/lanzador/Lanzador.java
@@ -15,5 +15,15 @@ public class Lanzador {
     public static void main(String[] args) {
 
 
+        try {
+//          Ejecuta el Productor una vez
+            Runtime.getRuntime().exec("java -jar Productor.jar");
+            for (int i = 0; i < 100; i++) {
+//          Ejecuta el Consumidor 100 veces con un identificador numerico como argumento 0
+                Runtime.getRuntime().exec("java -jar Consumidor.jar " + i);
+            }
+        } catch (IOException e) {
+            System.err.println("Error. " + e.toString());
+        }
     }
 }
