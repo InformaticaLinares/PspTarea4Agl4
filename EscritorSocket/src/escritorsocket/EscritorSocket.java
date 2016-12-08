1a0ddc22edb52d9b01d015325c1bb44cc20a8500
juan <juan@Juan-Asus>
2016-12-08 21:07:10 +0100

    Escritor escribe en consola en lugar de en un .txt

--- EscritorSocket/src/escritorsocket/EscritorSocket.java
+++ EscritorSocket/src/escritorsocket/EscritorSocket.java
@@ -18,21 +18,6 @@ public class EscritorSocket {//este es el proceso productor
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
-//      Fichero de logging
-        String fichlog = "javalogsum.txt";
-
-//      Se redirigen las salidas estandar
-        PrintStream ps = null;
-        try {
-            ps = new PrintStream(
-                    new BufferedOutputStream(new FileOutputStream(
-                            new File(fichlog), true)), true);
-        } catch (FileNotFoundException e) {
-            System.out.println(e.toString());
-        } finally {
-            System.setOut(ps);
-            System.setErr(ps);
-        }
 //Creo el server socket para la conexion de los consumidosres
 //      Fichero para buffer
         String fichruta = "buffer.txt";
@@ -41,36 +26,24 @@ public class EscritorSocket {//este es el proceso productor
         ServerSocket conexion = null; //Socket para aceptar conexiones
         Socket canal = null;   // Socket para establecer el canal
         PrintWriter salidaStream; //Stream de salida para el socket
-        try {
-            conexion = new ServerSocket(12502);
-            //Pido al SO que abra el puerto 12500 para la escucha de la conexion
-        } catch (IOException e) {
-            System.err.println("No se puede abrir el puerto");
-            System.err.print(e.toString());
-            System.exit(-1);
-        }
 
         while (contador < 100) {
             try {
-                System.out.println("Productor esperando conexión de consumidor");
-                canal = conexion.accept(); //productor espera que se conecte un consumidor
-         //     salidaStream = new PrintWriter(canal.getOutputStream());
-         //     Abrimos el fichero buffer   
-                File fbuffer = new File(fichruta);
-                RandomAccessFile raf = new RandomAccessFile(fbuffer, "rwd");
-                FileChannel channel = raf.getChannel();
-                FileLock lock = channel.lock();
-                System.out.println("Suministrador--> Entro en Región Crítica");
-//              Se sebreescribe el contador, para lo que nos ponemos al principio del fichero
-                raf.seek(0);
-                raf.writeInt(contador);
-                //Finalizacion del bloqueo de escritura en el fichero  
-                lock.release();
-                System.out.println("Suministrador--> Nuevo valor del contador escrito (" + contador + ") grabado. Salgo de la R.C.");
-//              Aumentamos el valor del contador
+                conexion = new ServerSocket(12502);
+                //Pido al SO que abra el puerto 12500 para la escucha de la conexion
+            } catch (IOException e) {
+                System.err.println("No se puede abrir el puerto");
+                System.err.print(e.toString());
+                System.exit(-1);
+            }
+            try {
+
+                salidaStream = new PrintWriter(canal.getOutputStream());
+                salidaStream.println(contador);
                 contador++;
-                channel.close();
-                raf.close();
+
+                salidaStream.flush();
+                salidaStream.close();
                 canal.close(); //cierro canal, puede elevar IOException
                 conexion.close();
             } //cierro el ServerSocket
@@ -81,5 +54,6 @@ public class EscritorSocket {//este es el proceso productor
                 System.err.print(e.toString());
             }
         }
+
     }
 }
